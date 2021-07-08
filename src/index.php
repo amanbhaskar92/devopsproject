d576afee7fe5fad73e84371b60bec7ee1801a9d3
amanbhaskar92 <68425827+amanbhaskar92@users.noreply.github.com>
2021-07-08 19:34:51 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Docker Swarm</p>
+                <p>Your PHP application is now running on a container in Kubernetes cluster setup by Aman Bhaskar</p>
                 <p>The Kubernetes Docker container is running on <?php echo gethostname() ?> with PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
